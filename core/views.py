74e426d9ab9611ea67f11b6ad3e761cf87a12644
samuel <studioproend@gmail.com>
2022-06-10 16:44:44 -0300

    add signup

--- core/views.py
+++ core/views.py
@@ -1,7 +1,10 @@
+from socket import INADDR_UNSPEC_GROUP
 from django.shortcuts import redirect, render
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
 from django.http import HttpResponse
+
+from .models import Profile
 
 
 
@@ -26,6 +29,16 @@ def signup(request):
                 user = User.objects.create_user(username=username, email=email, password=password)
                 user.save()
 
+                # log user in and redirect to settings page
+
+
+                #create a profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('signup')
+
+
         else:
             messages.info(request, 'Password Not Matching')
             return redirect('signup')
@@ -36,5 +49,6 @@ def signup(request):
         return render(request,'signup.html')
 
 
-
+def segnin(request):
+    return render(request, 'signin.html')
 
