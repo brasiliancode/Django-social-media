dffdc616c68ed2831fcf3a9de36b853aa0d827d4
samuel <studioproend@gmail.com>
2022-06-17 21:36:53 -0300

    like_post

--- core/views.py
+++ core/views.py
@@ -6,17 +6,56 @@ from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 
-from .models import Profile
+from .models import LikePost, Post, Profile
 
 @login_required(login_url='signin')
 def index(request):
     user_object = User.objects.get(username=request.user.username)
     user_profile = Profile.objects.get(user=user_object)
-    return render(request,'index.html', {'user_profile': user_profile})
+
+    posts = Post.objects.all()
+    return render(request,'index.html', {'user_profile': user_profile, 'posts':posts})
 
 @login_required(login_url='signin')
 def upload(request):
+
+
+    if request.method == 'POST':
+        user = request.user.username
+        image = request.FILES.get('image_upload')
+        caption = request.POST['caption']
+
+        new_post = Post.objects.create(user=user, image=image, caption=caption)
+        new_post.save()
+
+        return redirect('/')
+    else:
+        return redirect('/')
     return HttpResponse('<h1>Upload Views</h1>')
+
+@login_required(login_url='signin')
+def like_post(request):
+    username = request.user.username
+    post_id = request.GET.get('post_id')
+
+    post = Post.objects.get(id=post_id)
+
+    like_filter = LikePost.objects.filter(post_id=post_id, username=username).first()
+
+    if like_filter == None:
+        new_like = LikePost.objects.create(post_id=post_id, username=username)
+        new_like.save()
+        post.no_of_likes = post.no_of_likes+1
+        post.save()
+        return redirect('/')
+    else:
+        like_filter.delete()
+        post.no_of_likes = post.no_of_likes-1
+        post.save()
+        return redirect('/')
+
+
+
 
 @login_required(login_url='signin')
 def settings(request):
